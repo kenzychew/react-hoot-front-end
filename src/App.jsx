715dd015310cd894d5ed330f1a49283c5183798d
kenzychew <kenzychew@gmail.com>
2025-02-13 15:18:43 +0800

    feat: add protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import { useContext } from 'react';
-import { Routes, Route } from 'react-router';
+import { useContext } from "react";
+import { Route, Routes } from "react-router";
 
-import NavBar from './components/NavBar/NavBar';
-import SignUpForm from './components/SignUpForm/SignUpForm';
-import SignInForm from './components/SignInForm/SignInForm';
-import Landing from './components/Landing/Landing';
-import Dashboard from './components/Dashboard/Dashboard';
+import Dashboard from "./components/Dashboard/Dashboard";
+import Landing from "./components/Landing/Landing";
+import NavBar from "./components/NavBar/NavBar";
+import SignInForm from "./components/SignInForm/SignInForm";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
 
-import { UserContext } from './contexts/UserContext';
+import HootList from "./components/HootList/HootList";
+import { UserContext } from "./contexts/UserContext";
 
 const App = () => {
   const { user } = useContext(UserContext);
-  
+
   return (
     <>
-      <NavBar/>
+      <pre>{JSON.stringify(user)}</pre>
+      <NavBar />
       <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
+        <Route path="/" element={user ? <Dashboard /> : <Landing />} />
+        {user ? (
+          <>
+            {/* Protected routes (available only to signed-in users) */}
+            <Route path="/hoots" element={<HootList />} />
+          </>
+        ) : (
+          <>
+            {/* Non-user routes (available only to guests) */}
+            <Route path="/sign-up" element={<SignUpForm />} />
+            <Route path="/sign-in" element={<SignInForm />} />
+          </>
+        )}
+        <Route path="/*" element={<p>No such page</p>} />
       </Routes>
     </>
   );
